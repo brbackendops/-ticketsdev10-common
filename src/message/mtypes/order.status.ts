b8290ec97ac380a43b8d932b3d16b5fdbf453802
AnonymousXDragon <rohitAsAnonymous@proton.me>
2025-01-16 20:37:44 +0530

    Updates

--- src/message/mtypes/order.status.ts
+++ src/message/mtypes/order.status.ts
@@ -3,5 +3,6 @@ export enum OrderStatus {
     Created = "created",
     Complete = "complete",
     Cancelled = "cancelled",
-    AwaitingPayment = "awaiting:payment"    
+    AwaitingPayment = "awaiting:payment",
+    ChargeCreated = "charge:created"
 }
